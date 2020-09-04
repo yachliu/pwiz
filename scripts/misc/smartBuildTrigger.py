b5653a49483c4ba266ff27d75d96311632e9bc73
Matt Chambers <matt.chambers42@gmail.com>
2020-09-04 12:53:16 -0400

    installer testing

--- scripts/misc/smartBuildTrigger.py
+++ scripts/misc/smartBuildTrigger.py
@@ -63,50 +63,51 @@ if len(args) < 4:
     exit(0)
 
 targets = {}
-targets['CoreWindowsRelease'] = \
-{
-    "bt83": "Core Windows x86_64"
-    ,"bt36": "Core Windows x86"
-    ,"bt143": "Core Windows x86_64 (no vendor DLLs)"
-}
+#targets['CoreWindowsRelease'] = \
+#{
+#    "bt83": "Core Windows x86_64"
+#    ,"bt36": "Core Windows x86"
+#    ,"bt143": "Core Windows x86_64 (no vendor DLLs)"
+#}
 #targets['CoreWindowsDebug'] = \
 #{
 #    "bt84": "Core Windows x86_64 debug"
 #    ,"bt75": "Core Windows debug"
 #}
 #targets['CoreWindows'] = merge(targets['CoreWindowsRelease'], targets['CoreWindowsDebug'])
-targets['CoreWindows'] = targets['CoreWindowsRelease']
-targets['CoreLinux'] = {"bt17": "Core Linux x86_64"}
+#targets['CoreWindows'] = targets['CoreWindowsRelease']
+#targets['CoreLinux'] = {"bt17": "Core Linux x86_64"}
 
-targets['SkylineRelease'] = \
-{
-    "ProteoWizard_WindowsX8664msvcProfessionalSkylineResharperChecks": "Skyline code inspection" # depends on "bt209",
-    ,"bt209": "Skyline master and PRs (Windows x86_64)"
-    ,"bt19": "Skyline master and PRs (Windows x86)"
-}
+#targets['SkylineRelease'] = \
+#{
+#    "ProteoWizard_WindowsX8664msvcProfessionalSkylineResharperChecks": "Skyline code inspection" # depends on "bt209",
+#    ,"bt209": "Skyline master and PRs (Windows x86_64)"
+#    ,"bt19": "Skyline master and PRs (Windows x86)"
+#}
 #targets['SkylineDebug'] = \
 #{
 #    "bt210": "Skyline master and PRs (Windows x86_64 debug)"
 #    ,"bt87": "Skyline master and PRs (Windows x86 debug)"
 #}
 #targets['Skyline'] = merge(targets['SkylineRelease'], targets['SkylineDebug'])
-targets['Skyline'] = targets['SkylineRelease']
+#targets['Skyline'] = targets['SkylineRelease']
 
-targets['Container'] = \
-{
-    "ProteoWizardAndSkylineDockerContainerWineX8664": "ProteoWizard and Skyline Docker container (Wine x86_64)"
-}
+#targets['Container'] = \
+#{
+#    "ProteoWizardAndSkylineDockerContainerWineX8664": "ProteoWizard and Skyline Docker container (Wine x86_64)"
+#}
 
-targets['BumbershootRelease'] = \
-{
-    "Bumbershoot_Windows_X86_64": "Bumbershoot Windows x86_64"
-    ,"ProteoWizard_Bumbershoot_Windows_X86": "Bumbershoot Windows x86"
-}
-targets['BumbershootLinux'] = {"ProteoWizard_Bumbershoot_Linux_x86_64": "Bumbershoot Linux x86_64"}
-targets['Bumbershoot'] = merge(targets['BumbershootRelease'], targets['BumbershootLinux'])
+#targets['BumbershootRelease'] = \
+#{
+#    "Bumbershoot_Windows_X86_64": "Bumbershoot Windows x86_64"
+#    ,"ProteoWizard_Bumbershoot_Windows_X86": "Bumbershoot Windows x86"
+#}
+#targets['BumbershootLinux'] = {"ProteoWizard_Bumbershoot_Linux_x86_64": "Bumbershoot Linux x86_64"}
+#targets['Bumbershoot'] = merge(targets['BumbershootRelease'], targets['BumbershootLinux'])
 
-targets['Core'] = merge(targets['CoreWindows'], targets['CoreLinux'])
-targets['All'] = merge(targets['Core'], targets['Skyline'], targets['Bumbershoot'], targets['Container'])
+#targets['Core'] = merge(targets['CoreWindows'], targets['CoreLinux'])
+#targets['All'] = merge(targets['Core'], targets['Skyline'], targets['Bumbershoot'], targets['Container'])
+targets['CoreWindows'] = {"ProteoWizard_CoreWindowsX8664installerTest": "Installer test"}
 
 # Patterns are processed in order. If a path matches multiple patterns, only the first pattern will trigger. For example,
 # "pwiz_tools/Bumbershoot/Jamfile.jam" matches both "pwiz_tools/Bumbershoot/.*" and "pwiz_tools/.*", but will only trigger "Bumbershoot" targets
@@ -115,7 +116,7 @@ matchPaths = [
     #("libraries/.*", targets['All']),
     #("pwiz/.*", targets['All']),
     #("pwiz_aux/.*", targets['All']),
-    ("scripts/wix/.*", {"ProteoWizard_CoreWindowsX8664installerTest": "Installer test"}),# targets['CoreWindows']),
+    ("scripts/wix/.*", targets['CoreWindows']),
     #("scripts/.*", targets['All']),
     #("pwiz_tools/BiblioSpec/.*", merge(targets['Core'], targets['Skyline'], targets['Container'])),
     #("pwiz_tools/Bumbershoot/.*", targets['Bumbershoot']),
